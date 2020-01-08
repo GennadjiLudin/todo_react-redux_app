27be8ddd5911074f46599e0e2511448a12a1484d
garry1980 <ludingv@yandex.ru>
2020-01-08 19:23:11 +0500

    Updates

---
+++ precache-manifest.a711fcdd1d672ad92a25041b6f76af73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2df611b052332de984adea3221ec4b",
+    "url": "/todo_react-redux_app/index.html"
+  },
+  {
+    "revision": "fa949fd6880698b63072",
+    "url": "/todo_react-redux_app/static/css/main.4ef790b0.chunk.css"
+  },
+  {
+    "revision": "3a7a766f948a2dfab49b",
+    "url": "/todo_react-redux_app/static/js/2.e6bbb807.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo_react-redux_app/static/js/2.e6bbb807.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa949fd6880698b63072",
+    "url": "/todo_react-redux_app/static/js/main.c4a37f42.chunk.js"
+  },
+  {
+    "revision": "c65b49ac91e492116875",
+    "url": "/todo_react-redux_app/static/js/runtime-main.37cea1d7.js"
+  }
+]);
